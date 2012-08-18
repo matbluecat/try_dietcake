3457429d46305f346285cd3fc06051a220551ab9
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-18 15:39:06 +0900

    Import dietcake-hello

---
+++ app/config/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+// application
+require_once APP_DIR.'app_controller.php';
+require_once APP_DIR.'app_model.php';
+require_once APP_DIR.'app_exception.php';
+
+// helpers
+require_once HELPERS_DIR.'html_helper.php';
+
+spl_autoload_register(function($name) {
+    $filename = Inflector::underscore($name) . '.php';
+    if (strpos($name, 'Controller') !== false) {
+        require CONTROLLERS_DIR . $filename;
+    } else {
+        if (file_exists(MODELS_DIR . $filename)) {
+            require MODELS_DIR . $filename;
+        }
+    }
+});
