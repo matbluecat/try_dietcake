2909607c4bf35e8d33930f178da2fcc584898e97
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-18 17:13:04 +0900

    Add database scheme

---
+++ app/config/sql/message-board.sql
@@ -0,0 +1,31 @@
+--
+-- Create database
+--
+CREATE DATABASE IF NOT EXISTS board;
+GRANT SELECT, INSERT, UPDATE, DELETE ON board.* TO board_root@localhost IDENTIFIED BY 'board_root';
+FLUSH PRIVILEGES;
+
+--
+-- Create tables
+--
+
+USE board;
+
+CREATE TABLE IF NOT EXISTS thread (
+id                      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+username                VARCHAR(255) NOT NULL,
+title                   VARCHAR(255) NOT NULL,
+body                    TEXT NOT NULL,
+created                 DATETIME NOT NULL,
+PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS comment (
+id                      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+thread_id               INT UNSIGNED NOT NULL,
+username                VARCHAR(255) NOT NULL,
+body                    TEXT NOT NULL,
+created                 DATETIME NOT NULL,
+PRIMARY KEY (id),
+INDEX (thread_id, created)
+)ENGINE=InnoDB;
