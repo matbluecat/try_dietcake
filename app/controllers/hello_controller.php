4aba7edae009c7e7343ec33ded74d1f9b5f3db5f
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-19 17:27:50 +0900

    Remove unused files

--- app/controllers/hello_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class HelloController extends AppController
-{
-    public function index()
-    {
-        $message = Hello::getMessage();
-        $this->set(get_defined_vars());
-    }
-}
--- app/models/hello.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Hello extends AppModel
-{
-    public static function getMessage()
-    {
-        return 'hello world!';
-    }
-}
