45c26e72c52f0c4e7a790673f525e66d0fafd86f
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-19 15:38:31 +0900

    Add comment-form

--- app/views/thread/view.php
+++ app/views/thread/view.php
@@ -10,3 +10,17 @@
   </div>
 </div>
 <?php endforeach ?>
+
+
+<hr>
+
+<form class="well" method="post" action="<?php eh(url('thread/write')) ?>">
+  <label>Your name</label>
+  <input type="text" class="span2" name="username"  value="<?php eh(Param::get('username')) ?>">
+  <label>Comment</label>
+  <textarea name="body"<?php eh(Param::get('body')) ?>></textarea>
+  <br />
+  <input type="hidden" name="thread_id" value="<?php eh($thread->id) ?>">
+  <input type="hidden" name="page_next" value="write_end">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
