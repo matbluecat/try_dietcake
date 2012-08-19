f5f72a32b2699517675b9df175c3429faa1d229a
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-19 15:25:10 +0900

    Fix db scheme

--- app/config/sql/message-board.sql
+++ app/config/sql/message-board.sql
@@ -15,7 +15,6 @@ CREATE TABLE IF NOT EXISTS thread (
 id                      INT UNSIGNED NOT NULL AUTO_INCREMENT,
 username                VARCHAR(255) NOT NULL,
 title                   VARCHAR(255) NOT NULL,
-body                    TEXT NOT NULL,
 created                 DATETIME NOT NULL,
 PRIMARY KEY (id)
 )ENGINE=InnoDB;
