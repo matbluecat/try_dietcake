506bc779f867589a5df21343b34f9d63ed714c19
Tatsuya Tsuruoka <tatsuya.tsuruoka@gmail.com>
2012-08-21 12:44:11 +0900

    Update DB class

--- app/config/database.php
+++ app/config/database.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * DBI class for DietCake
+ *
+ * @license MIT License
+ * @author Tatsuya Tsuruoka <http://github.com/ttsuruoka>
+ * @link https://github.com/dietcake/dietcake-showcase
+ */
+
 class DB extends SimpleDBI
 {
     public static function getConnectSetting($destination = null)
@@ -13,5 +21,24 @@ class DB extends SimpleDBI
         }
 
         return array($dsn, $username, $password, $driver_options);
+    }
+
+    protected function onQueryEnd($sql, array $params = array())
+    {
+        $this->log($sql, $params);
+    }
+
+    protected function log($sql, array $params = array())
+    {
+        if (ENV_PRODUCTION) {
+            return;
+        }
+
+        static $num_query = 1;
+
+        preg_match('/host=([0-9A-Za-z_-]+)/', $this->dsn, $matches);
+        $host = isset($matches[1]) ? $matches[1] : '-';
+
+        Log::debug(sprintf("sql\t%f\t%d:(%s) %s; (%s)", $this->getLastExecTime(), $num_query++, $host, $sql, implode(', ', $params)), 'sql');
     }
 }
